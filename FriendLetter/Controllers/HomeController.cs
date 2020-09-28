618c4bda93a721b7164b0f9f5d95790941afab87
3Emme <emmettbuentiempo@gmail.com>
2020-09-28 09:30:38 -0700

    add View and write Letter.cshtml.
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -4,7 +4,13 @@ namespace FriendLetter.Controllers
 {
   public class HomeController : Controller
   {
-      public string Hello() { return "Hello friend!"; }
-      public string Goodbye() { return "Goodbye friend."; }
+      [Route("/hello")]
+    public string Hello() { return "Hello friend!"; }
+
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+    [Route("/")]
+    public ActionResult Letter() { return View(); }
   }
 }
